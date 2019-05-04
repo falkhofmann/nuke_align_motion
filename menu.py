802def760a4bc32c372f80a22b757651d2cf2cc1
Falk Hofmann <falk@kombinat-13b.de>
2019-05-04 08:31:12 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -6,10 +6,4 @@ from nuke_align_motion import controller
 menubar = nuke.menu('Nuke')
 toolbar = nuke.menu('Nodes')
 for menu in menubar, toolbar:
-    if menu == menubar:
-        fhofmann = menu.addMenu("fhofmann")
-    else:
-        fhofmann = menu.addMenu("fhofmann")
-
-    fhofmann.addCommand("motion align", 'controller.start()', "f3")
-
+    menu.addMenu("fhofmann").addCommand("motion align", 'controller.start()', "f3")
